4b9bf674b528d0d2977284ae188c8e78093e3c92
bomotayo <omotayobabatundee@gmail.com>
2021-11-13 10:05:06 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {lorem} from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+  title = 'Typing Challenge';
+  randomText=lorem.sentence();
+  typedText="";
+
+  onInput(event:Event){
+    const target = event.target as HTMLInputElement;
+
+    this.typedText = target.value;
+  }
+
+  compare(letter:string, text:string){
+    if(!text){
+      return 'pending'
+    }
+    return letter === text ? 'correct' : 'incorrect';
+  }
 }
